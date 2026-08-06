5a26baf47e4ba33c5aae8acec39a8db0f75b4972
luwenrui3055-source <luwenrui3055@gmail.com>
2026-08-06 19:33:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+process.env.BARK_KEY = 'ga9SV7GU3jDrqpAbVrdjkC';
+process.env.PUSH_PROVIDER = 'bark';
+process.env.TARGET_API_URL = 'https://ckffai.com/v1/chat/completions';
+process.env.TARGET_API_KEY = 'sk-nvzSNQcmraZqOlhap7NlfJEFH9PZVav7Sf7Ba6F6BlEOOcfJ';
+process.env.MODEL_NAME = 'gemini-2.5-pro';
+process.env.GATEWAY_API_KEY = 'saku2026heartbeat';
+process.env.ALLOW_PUBLIC_API = 'true';
+process.env.TIME_ZONE = 'Asia/Shanghai';
+process.env.ADMIN_USER = 'admin';
+process.env.ADMIN_PASSWORD = 'saku123';
+process.env.PORT = '8080';
+
 require("dotenv").config();
 
 const Fastify = require("fastify");
