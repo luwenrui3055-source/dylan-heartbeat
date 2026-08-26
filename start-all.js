4d2e9e2f2bb75b69a3f2ae34fee83920a3acecb8
luwenrui3055-source <luwenrui3055@gmail.com>
2026-08-26 19:45:32 +0800

    Update start-all.js

--- start-all.js
+++ start-all.js
@@ -1,10 +1,7 @@
 const { spawn } = require('child_process');
 
 // 手动注入环境变量
-process.env.TARGET_API_URL = 'https://ckffai.com/v1/chat/completions';
-process.env.TARGET_API_KEY = 'sk-nvzSNQcmraZqOlhap7NlfJEFH9PZVav7Sf7Ba6F6BlEOOcfJ';
-process.env.MODEL_NAME = 'gemini-2.5-pro';
-process.env.GATEWAY_API_KEY = 'saku2026heartbeat';
+
 process.env.ALLOW_PUBLIC_API = 'true';
 process.env.PUSH_PROVIDER = 'bark';
 process.env.BARK_KEY = 'ga9SV7GU3jDrqpAbVrdjkC';
