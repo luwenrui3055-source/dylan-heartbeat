9b0785e217149445c66ecf4d1c1980578635279a
luwenrui3055-source <luwenrui3055@gmail.com>
2026-08-26 19:49:49 +0800

    Update start-all.js

--- start-all.js
+++ start-all.js
@@ -2,14 +2,6 @@ const { spawn } = require('child_process');
 
 // 手动注入环境变量
 
-process.env.ALLOW_PUBLIC_API = 'true';
-process.env.PUSH_PROVIDER = 'bark';
-process.env.BARK_KEY = 'ga9SV7GU3jDrqpAbVrdjkC';
-process.env.TIME_ZONE = 'Asia/Shanghai';
-process.env.ADMIN_USER = 'admin';
-process.env.ADMIN_PASSWORD = 'saku123';
-process.env.PORT = '8080';
-
 const server = spawn('node', ['server.js'], { stdio: 'inherit', env: process.env });
 const wakeup = spawn('node', ['wake_up.js'], { stdio: 'inherit', env: process.env });
 
